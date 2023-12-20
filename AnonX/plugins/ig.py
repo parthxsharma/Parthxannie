71fb0c044bdcb046d400a502dc24aafaf0485626
crioctic <154237034+crioctic@users.noreply.github.com>
2023-12-20 23:02:32 +0530

    Update ig.py

--- AnonX/plugins/ig.py
+++ AnonX/plugins/ig.py
@@ -15,6 +15,36 @@ headers = {
     "Connection": "keep-alive",
     "Referer": "https://saveig.app/en",
 }
+
+@app.on_message(filters.regex(r'https?://.*facebook[^\s]+') & filters.incoming,group=-6)
+async def link_handler(app, message):
+    link = message.matches[0].group(0)
+    try:
+       m = await message.reply_text("⏳")
+       get_api=requests.get(f"https://yasirapi.eu.org/fbdl?link={link}").json()
+       if get_api['success'] == "false":
+          return await message.reply("Invalid TikTok video url. Please try again :)")
+       if get_api['success'] == "ok":
+          if get_api.get('result').get('hd'):
+             try:
+                 dump_file = await message.reply_video(get_api['result']['hd'],caption="Thank you for using")
+             except KeyError:
+                 pass 
+             except Exception:
+                 try:
+                     sndmsg = await message.reply(get_api['result']['hd'])
+                     await asyncio.sleep(1)
+                     dump_file = await message.reply_video(get_api['result']['hd'],caption="Thank you for using")
+                     await sndmsg.delete()
+                 except Exception:
+                     try:
+                        down_file = wget.download(get_api['result']['hd'])
+                        await message.reply_video(down_file,caption="Thank you for using")
+                        await sndmsg.delete()
+                        os.remove(down_file)
+                     except:
+                         return await message.reply("Oops Failed To Send File Instead Of Link")
+
 @app.on_message(filters.regex(r'https?://.*instagram[^\s]+') & filters.incoming)
 async def link_handler(app, message):
     link = message.matches[0].group(0)
