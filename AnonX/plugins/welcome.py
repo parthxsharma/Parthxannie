b6a87aa5d100159c9422d9efd406ff66e0404163
crioctic <154237034+crioctic@users.noreply.github.com>
2023-12-21 15:14:59 +0530

    Update welcome.py

--- AnonX/plugins/welcome.py
+++ AnonX/plugins/welcome.py
@@ -117,15 +117,3 @@ Usᴇʀɴᴀᴍᴇ ✧ @{user.username}
 
  
 
-@app.on_message(filters.new_chat_members & filters.group, group=-1)
-async def bot_wel(app, message):
-    for u in message.new_chat_members:
-        if u.id == app.me.id:
-            await app.send_message(LOG_GROUP_ID, f"""
-**NEW GROUP
-➖➖➖➖➖➖➖➖➖➖➖➖
-NAME: {message.chat.title}
-ID: {message.chat.id}
-USERNAME: @{message.chat.username}
-➖➖➖➖➖➖➖➖➖➖➖➖**
-""")
